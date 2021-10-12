34b09dbdcb251da798dee05ca2d8690cf3b51269
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-12 14:18:04 -0700

    added post and put method

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ app.use(cors());
 const ctrl = require('./controller');
 
 app.get('/api/houses', ctrl.getHouses);
+app.post('/api/houses', ctrl.createHouses);
+app.put('/api/houses/:id', ctrl.updateHouse);
 
 app.listen(4545, () => 
 console.log ("4545 running!"));
