26e8f53bc5ed4ede88b76b4e716c57f331a3d3e7
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-12 12:56:41 -0700

    created express

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(4004, () => 
+console.log ("4004 running!"))
