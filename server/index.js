ebef6dac5ac8bc6f8991fef9cf82306894a8f0d0
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-12 13:18:42 -0700

    add get method

--- server/index.js
+++ server/index.js
@@ -6,5 +6,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(4004, () => 
-console.log ("4004 running!"))
+const ctrl = require('./controller');
+
+app.get('/api/houses', ctrl.getHouses);
+
+app.listen(4545, () => 
+console.log ("4545 running!"));
